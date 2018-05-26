61a52563d9c8f2051372f1c0cb7b0ef31f4231ea
PatODay <poday22@gmail.com>
2018-05-26 12:52:17 -0400

    Generated Model task
    Generated model for `task` with `ember generate model task`
    Added values for name, category, date, is_completed, location, and description with DS.attr.
    Added ownership value for user with user: DS.belongsTo('user')

---
+++ app/models/task.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  category: DS.attr('string'),
+  date: DS.attr('date'),
+  is_completed: DS.attr('boolean'),
+  location: DS.attr('string'),
+  description: DS.attr('string'),
+  user: DS.belongsTo('user')
+});
