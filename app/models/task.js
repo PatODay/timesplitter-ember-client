a74d11411e39ef97d0843ef2fd096ba2f22083b7
PatODay <poday22@gmail.com>
2018-05-29 15:12:31 -0400

    Bug Fix for date
    Fixed issue where the date being passed to the API was 'null'
    In app/models/task.js changed `DSattr('date')` to DSattr('string')
    Re-added date to the update-task.hbs to be rendered to the user

--- app/models/task.js
+++ app/models/task.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   category: DS.attr('string'),
-  date: DS.attr('date'),
+  date: DS.attr('string'),
   is_completed: DS.attr('boolean'),
   location: DS.attr('string'),
   description: DS.attr('string'),
