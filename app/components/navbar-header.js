427515aa53b9d1e13c83ec8960456aa12dd88bb2
PatODay <poday22@gmail.com>
2018-05-30 16:47:12 -0400

    Hiding link-to tasks for non-auth users
    In templates/components/navbar-header.hbs added `if` statement to hide link to tasks unless user isAuthenticated
    In app/components/navbar-header.js imported `alias` and `service`
      * Also Added ```auth: service(),
      isAuthenticated: alias('auth.isAuthenticated')```

--- app/components/navbar-header.js
+++ app/components/navbar-header.js
@@ -1,6 +1,10 @@
 import Component from '@ember/component'
+import { alias } from '@ember/object/computed'
+import { inject as service } from '@ember/service'
 
 export default Component.extend({
   tagName: 'div',
-  classNames: ['navbar-header']
+  classNames: ['navbar-header'],
+  auth: service(),
+  isAuthenticated: alias('auth.isAuthenticated')
 })
